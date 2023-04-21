5ba8a4a203171ee1eb9485e9f85e19900199a477
GvantsaMaisuradze <gvantsamaisuradze14@gmail.com>
2023-04-21 15:13:24 +0400

    home-pg

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePgComponent } from './view/home-pg/home-pg.component';
+
+const routes: Routes = [
+  {path:"",component:HomePgComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
